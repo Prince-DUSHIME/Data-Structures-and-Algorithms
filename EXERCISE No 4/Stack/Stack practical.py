86599ce7d2ac16aafa2b14b8affff25da7e0147c
Prince DUSHIME <147556134+Prince-DUSHIME@users.noreply.github.com>
2025-09-25 16:47:17 +0200

    Add files via upload

---
+++ EXERCISE No 4/Stack/Stack practical.py
@@ -0,0 +1,16 @@
+# Queue parctical 1
+stack = ["Enter PIN", "Enter Amount", "Confirm"]
+print("Initial stack:", stack)
+
+stack.pop()  
+stack.pop() 
+
+print("After popping twice:", stack)
+
+# Stack practical 2
+stack = ["Start Exam", "Answer Q1", "Answer Q2"]
+print("Initial stack:", stack)
+
+stack.pop()  
+
+print("After one pop:", stack)
